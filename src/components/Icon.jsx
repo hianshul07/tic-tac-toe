a76897dc9cbeb98ebea7a35cd2606ad0bc2ac65b
Anshul Kumar <hianshul07@gmail.com>
2023-03-13 21:50:21 +0530

    fa icons added

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { FaRegCircle, FaTimes, FaPen } from "react-icons/fa";
 
-function Icon ({ name }) {
-    return (
-        <h1>Hello</h1>
-    )
+function Icon({ name }) {
+	return <h1>Hello</h1>;
 }
 
-export default Icon
+export default Icon;
