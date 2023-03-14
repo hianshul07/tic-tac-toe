fec2429b096a8e49a241f6022a4e662421a798ca
Anshul Kumar <hianshul07@gmail.com>
2023-03-14 19:21:14 +0530

    css issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./components/Icon";
+import Icon from "./components/Icon";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { Card, CardBody, Col, Row, Button, Container } from "reactstrap";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [count, setCount] = useState(0);
+	const [isCross, setIsCross] = useState(false);
+	const [winMessage, setWinMessage] = useState("");
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+	const reloadGame = () => {};
 
-export default App
+	const checkIsWinner = () => {};
+
+	const itemChanged = (itemNumber) => {};
+
+	const itemArray = new Array(9).fill("empty");
+
+	return (
+
+		<Container className="p-5">
+			<ToastContainer position="bottom-center" />
+			<Row>
+				<Col md={6} className="offset-md-3">
+					<div className="grid">
+						{itemArray.map((item, index) => (
+							<Card>
+								<CardBody className="box">
+									<Icon />
+								</CardBody>
+							</Card>
+						))}
+					</div>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+export default App;
+
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,7 +3,19 @@ import ReactDOM from "react-dom";
 import { FaRegCircle, FaTimes, FaPen } from "react-icons/fa";
 
 function Icon({ name }) {
-	return <h1>Hello</h1>;
+	return (
+		<div>
+			<FaPen />
+		</div>
+	);
+	switch (name) {
+		case "circle":
+			return <FaRegCircle className="icons"/>;
+		case "cross":
+			return <FaTimes className="icons"/>;
+		default:
+			return <FaPen className="icons"/>;
+	}
 }
 
 export default Icon;
